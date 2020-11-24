3ca0a5d5839f3c25904d385fe824b9b048c4385d
Rahaf Alenezi <ralenezi71@gmail.com>
2020-11-24 20:27:22 +0300

    update has errors

--- controllers/todosController.js
+++ controllers/todosController.js
@@ -1,35 +1,36 @@
-let todos = require("../todos");
-const slugify = require("slugify");
+let todos = require('../todos')
+const slugify = require('slugify')
 
 exports.todoCreate = (req, res) => {
-  const id = todos[todos.length - 1].id + 1;
-  const slug = slugify(req.body.name, { lower: true });
-  const newTodo = { id, slug, ...req.body }; // id, slug are equivalent to id: id, slug: slug
-  todos.push(newTodo);
-  res.status(201).json(newTodo);
-};
+  const id = todos[todos.length - 1].id + 1
+  const slug = slugify(req.body.name, { lower: true })
+  const newTodo = { id, slug, ...req.body } // id, slug are equivalent to id: id, slug: slug
+  todos.push(newTodo)
+  res.status(201).json(newTodo)
+}
 
-exports.todoList = (req, res) => res.json(todos);
+exports.todoList = (req, res) => res.json(todos)
 
-// exports.todoUpdate = (req, res) => {
-//   const { todoId } = req.params;
-//   const foundTodo = todos.find((todo) => todo.id === +todoId);
-//   if (foundTodo) {
-//     for (const key in req.body) foundTodo[key] = req.body[key];
-//     foundTodo.slug = slugify(req.body.name, { lower: true });
-//     res.status(204).end();
-//   } else {
-//     res.status(404).json({ message: "Todo not found" });
-//   }
-// };
+exports.todoUpdate = (req, res) => {
+  const { todoId } = req.params
+  const foundTodo = todos.find((todo) => todo.id === +todoId)
+  if (foundTodo) {
+    for (const key in req.body) foundTodo[key] = req.body[key]
+    // foundTodo.slug = slugify(req.body.name, { lower: true })
+    foundTodo.status = !foundTodo.status
+    res.status(204).end()
+  } else {
+    res.status(404).json({ message: 'Todo not found' })
+  }
+}
 
 exports.todoDelete = (req, res) => {
-  const { todoId } = req.params;
-  const foundTodo = todos.find((todo) => todo.id === +todoId);
+  const { todoId } = req.params
+  const foundTodo = todos.find((todo) => todo.id === +todoId)
   if (foundTodo) {
-    todos = todos.filter((todo) => todo !== foundTodo);
-    res.status(204).end();
+    todos = todos.filter((todo) => todo !== foundTodo)
+    res.status(204).end()
   } else {
-    res.status(404).json({ message: "Todo not found" });
+    res.status(404).json({ message: 'Todo not found' })
   }
-};
+}
--- todos.js
+++ todos.js
@@ -1,12 +1,12 @@
 const todos = [
   {
-    name: "finish math homework",
-    slug: "finish-math-homework",
-    status: "doing",
-    priority: "high",
+    name: 'finish math homework',
+    slug: 'finish-math-homework',
+    status: true,
+    priority: 'high',
     id: 1,
   },
-  { name: "complete science ", status: "done", priority: "high", id: 2 },
-  { name: "go to office hours", status: "not started", priority: "low", id: 3 },
-];
-module.exports = todos;
+  { name: 'complete science ', status: false, priority: 'high', id: 2 },
+  { name: 'go to office hours', status: false, priority: 'low', id: 3 },
+]
+module.exports = todos
