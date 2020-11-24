764897e71817667d664338a3f6ece56f992e5e69
saraadelalhaddad <sara.alhaddad@colorado.edu>
2020-11-24 17:16:32 +0300

    updates

--- todos.js
+++ todos.js
@@ -1,5 +1,11 @@
 const todos = [
-  { name: "finish math homework", status: "doing", priority: "high", id: 1 },
+  {
+    name: "finish math homework",
+    slug: "finish-math-homework",
+    status: "doing",
+    priority: "high",
+    id: 1,
+  },
   { name: "complete science ", status: "done", priority: "high", id: 2 },
   { name: "go to office hours", status: "not started", priority: "low", id: 3 },
 ];
